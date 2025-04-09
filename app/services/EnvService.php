2f8188277fee0b3cdd09ae2df70332649adc6888
tnb1604 <damisavooges2549@gmail.com>
2025-04-09 16:24:45 +0200

    gyat

--- app/services/EnvService.php
+++ app/services/EnvService.php
@@ -10,11 +10,11 @@ class EnvService
         $_ENV["MYSQL_HOST"] = "mysql.railway.internal";
         $_ENV["MYSQL_DATABASE"] = "railway";
         $_ENV["MYSQL_USER"] = "root";
-        $_ENV["MYSQL_PASSWORD"] = "QthwvNaULUiZXuyReCjjpxjfNRrpxrlS";
+        $_ENV["MYSQL_PASSWORD"] = "laYzFqVRqEmavMRfxctHnKAIgmRvTClY";
         $_ENV["MYSQL_CHARSET"] = "utf8mb4";
 
         $_ENV["ENV"] = "production";
         $_ENV["JWT_SECRET"] = "nLJ9gbjAMhkIViQ1AHB+yJIajSFmQ0vnJ3x3Vjvnprs=";
-        $_ENV["MYSQL_URL"] = "mysql://root:QthwvNaULUiZXuyReCjjpxjfNRrpxrlS@mysql.railway.internal:3306/railway";
+        $_ENV["MYSQL_URL"] = "mysql://root:laYzFqVRqEmavMRfxctHnKAIgmRvTClY@mysql.railway.internal:3306/railway";
     }
 }
